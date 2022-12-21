c5fcbfe6bc439e2db8726ec383fd06b2f305f09a
Yohan Kim <ddalpange@gmail.com>
2022-12-21 12:15:49 +0900

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,13 +114,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `YH Logs`,
+        short_name: `YH`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
